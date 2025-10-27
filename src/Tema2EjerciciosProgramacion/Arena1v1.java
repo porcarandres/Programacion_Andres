830170b09d0f39e9736a99b17558d54cfecbee7c
porcarandres <porcarandres07@gmail.com>
2025-10-27 09:45:25 +0100

    inicial 2

--- src/Tema2EjerciciosProgramacion/Arena1v1.java
+++ src/Tema2EjerciciosProgramacion/Arena1v1.java
@@ -21,36 +21,36 @@ public class Arena1v1 {
         opcion = sc.nextInt();
 
         if (opcion == 0) {
-            ataque1 = random.nextInt(120);
-            defensa1 = random.nextInt(120);
-            vida1 = random.nextInt(120);
-            velocidad1 = random.nextInt(120);
+            ataque1 = random.nextInt(200);
+            defensa1 = random.nextInt(170);
+            vida1 = random.nextInt(200);
+            velocidad1 = random.nextInt(170);
             System.out.println("Atributos aleatorios para " + jugador1 + ":");
             System.out.println("Ataque: " + ataque1 + " | Defensa: " + defensa1 + " | Vida: " + vida1 + " | Velocidad: " + velocidad1);
 
         } else {
-        do{
-            System.out.println(jugador1+" inserte sus atributos (no te pases, de entre 1-200 en cada y maximo entre todos 500");
-            System.out.println("ATAQUE J1");
-            ataque1 = sc.nextInt();
+            do {
+                System.out.println(jugador1 + " inserte sus atributos (no te pases, de entre 1-200 en cada y maximo entre todos 500");
+                System.out.println("ATAQUE J1");
+                ataque1 = sc.nextInt();
 
-            System.out.println("DEFENSA J1");
-            defensa1 = sc.nextInt();
+                System.out.println("DEFENSA J1");
+                defensa1 = sc.nextInt();
 
-            System.out.println("VIDA J1");
-            vida1 = sc.nextInt();
+                System.out.println("VIDA J1");
+                vida1 = sc.nextInt();
 
-            System.out.println("VELOCIDAD J1");
-            velocidad1 = sc.nextInt();
+                System.out.println("VELOCIDAD J1");
+                velocidad1 = sc.nextInt();
 
-            if (velocidad1 < 1 || velocidad1 > 200 || vida1 < 1 || vida1 > 200 || defensa1 < 1 || defensa1 > 200 || ataque1 < 1 || ataque1 > 200) {
-                System.out.println("no te pases de 200 en cada atributo");
-            } else if (velocidad1 + vida1 + defensa1 + ataque1 > 500) {
-                System.out.println("no te pases de 500 entre todos");
-            }
+                if (velocidad1 < 1 || velocidad1 > 200 || vida1 < 1 || vida1 > 200 || defensa1 < 1 || defensa1 > 200 || ataque1 < 1 || ataque1 > 200) {
+                    System.out.println("no te pases de 200 en cada atributo");
+                } else if (velocidad1 + vida1 + defensa1 + ataque1 > 500) {
+                    System.out.println("no te pases de 500 entre todos");
+                }
 
-        }while(vida1 < 1 || vida1 > 200 || velocidad1 < 1 || velocidad1 > 200 || defensa1 < 1 || defensa1 > 200 || ataque1 < 1 || ataque1 > 200);
-
+            } while (vida1 < 1 || vida1 > 200 || velocidad1 < 1 || velocidad1 > 200 || defensa1 < 1 || defensa1 > 200 || ataque1 < 1 || ataque1 > 200);
+        }
         //Jugador2
         System.out.println("Jugador2 inserte su nombre:");
         String jugador2 = sc.next();
@@ -58,36 +58,36 @@ public class Arena1v1 {
             System.out.println("¿Quieres que los atributos sean aleatorios? (0 = sí, otro número = manual)");
             opcion = sc.nextInt();
             if (opcion == 0) {
-                ataque2 = random.nextInt(120);
-                defensa2 = random.nextInt(120);
-                vida2 = random.nextInt(120);
-                velocidad2 = random.nextInt(120) ;
-                System.out.println("Atributos aleatorios para " + jugador1 + ":");
+                ataque2 = random.nextInt(200);
+                defensa2 = random.nextInt(170);
+                vida2 = random.nextInt(200);
+                velocidad2 = random.nextInt(170) ;
+                System.out.println("Atributos aleatorios para " + jugador2 + ":");
                 System.out.println("Ataque: " + ataque2 + " | Defensa: " + defensa2 + " | Vida: " + vida2 + " | Velocidad: " + velocidad2);
             } else {
-        do{
+                do {
 
-            System.out.println(jugador2+" inserte sus atributos (no te pases, de entre 1-200 en cada y maximo entre todos 500");
-            System.out.println("ATAQUE J2");
-            ataque2 = sc.nextInt();
+                    System.out.println(jugador2 + " inserte sus atributos (no te pases, de entre 1-200 en cada y maximo entre todos 500");
+                    System.out.println("ATAQUE J2");
+                    ataque2 = sc.nextInt();
 
-            System.out.println("DEFENSA J2");
-            defensa2 = sc.nextInt();
+                    System.out.println("DEFENSA J2");
+                    defensa2 = sc.nextInt();
 
-            System.out.println("VIDA J2");
-            vida2 = sc.nextInt();
+                    System.out.println("VIDA J2");
+                    vida2 = sc.nextInt();
 
-            System.out.println("VELOCIDAD J2");
-            velocidad2 = sc.nextInt();
+                    System.out.println("VELOCIDAD J2");
+                    velocidad2 = sc.nextInt();
 
-            if (velocidad2 < 1 || velocidad2 > 200 || vida2 < 1 || vida2 > 200 || defensa2 < 1 || defensa2 > 200 || ataque2 < 1 || ataque2 > 200) {
-                System.out.println("no te pases de 200 en cada atributo");
-            } else if (velocidad2 + vida2 + defensa2 + ataque2 > 500) {
-                System.out.println("no te pases de 500 entre todos");
+                    if (velocidad2 < 1 || velocidad2 > 200 || vida2 < 1 || vida2 > 200 || defensa2 < 1 || defensa2 > 200 || ataque2 < 1 || ataque2 > 200) {
+                        System.out.println("no te pases de 200 en cada atributo");
+                    } else if (velocidad2 + vida2 + defensa2 + ataque2 > 500) {
+                        System.out.println("no te pases de 500 entre todos");
+                    }
+
+                } while (vida2 < 1 || vida2 > 200 || velocidad2 < 1 || velocidad2 > 200 || defensa2 < 1 || defensa2 > 200 || ataque2 < 1 || ataque2 > 200);
             }
-
-        }while(vida2 < 1 || vida2 > 200 || velocidad2 < 1 || velocidad2 > 200 || defensa2 < 1 || defensa2 > 200 || ataque2 < 1 || ataque2 > 200);
-
         //Personajes Creados
         System.out.println("Personajes creados:");
         System.out.println("P1:"+jugador1+"//Atributos: || Vida"+vida1+"|| Ataque:"+ataque1+"|| Velocidad:"+velocidad1+"|| Defensa:"+defensa1);
@@ -107,28 +107,25 @@ public class Arena1v1 {
 
                 // Turno del jugador 1
 
-                System.out.println(jugador1+":"+vida1+" "+"-".repeat(vida1));
-                System.out.println(jugador2+":"+vida2+" "+"-".repeat(vida2));
+                System.out.println(jugador1 + ":" + vida1 + " " + "-".repeat(vida1));
+                System.out.println(jugador2 + ":" + vida2 + " " + "-".repeat(vida2));
 
-                d1 = (ataque1 - defensa2+ random.nextInt(10));
+                d1 = Math.max(5, ataque1 - defensa2 / 2 + random.nextInt(30));
                 vida2 -= d1;
                 System.out.println(jugador1 + " ataca a " + jugador2 + " causando " + d1 + " de daño. Vida restante de " + jugador2 + ": " + Math.max(0, vida2));
 
-                System.out.println(jugador1+":"+vida1+" "+"-".repeat(vida1));
-                System.out.println(jugador2+":"+vida2+" "+"-".repeat(vida2));
 
-                if (vida2 <= 0) break;
+                if (vida2 <= 0) {
+                    break;}
                 // Turno del jugador 2
 
-                System.out.println(jugador1+":"+vida1+" "+"-".repeat(vida1));
-                System.out.println(jugador2+":"+vida2+" "+"-".repeat(vida2));
+                System.out.println(jugador1 + ":" + vida1 + " " + "-".repeat(vida1));
+                System.out.println(jugador2 + ":" + vida2 + " " + "-".repeat(vida2));
 
-                d2 = (ataque2 -defensa1+ random.nextInt(10));
+                d2 = Math.max(5,ataque2 - defensa1/2 + random.nextInt(30));
                 vida1 -= d2;
                 System.out.println(jugador2 + " ataca a " + jugador1 + " causando " + d2 + " de daño. Vida restante de " + jugador1 + ": " + Math.max(0, vida1));
 
-                System.out.println(jugador1+":"+vida1+" "+"-".repeat(vida1));
-                System.out.println(jugador2+":"+vida2+" "+"-".repeat(vida2));
 
             } else {
                 System.out.print("Escribe siguiente para continuar: ");
@@ -136,39 +133,34 @@ public class Arena1v1 {
 
                 // Turno del jugador 2
 
-                System.out.println(jugador1+":"+vida1+" "+"-".repeat(vida1));
-                System.out.println(jugador2+":"+vida2+" "+"-".repeat(vida2));
+                System.out.println(jugador1 + ":" + vida1 + " " + "-".repeat(vida1));
+                System.out.println(jugador2 + ":" + vida2 + " " + "-".repeat(vida2));
 
-                d2 =(ataque2 - defensa1+ random.nextInt(10));
+                d2 = Math.max(5,ataque2 - defensa1/2 + random.nextInt(30));
                 vida1 -= d2;
                 System.out.println(jugador2 + " ataca a " + jugador1 + " causando " + d2 + " de daño. Vida restante de " + jugador1 + ": " + Math.max(0, vida1));
 
-                System.out.println(jugador1+":"+vida1+" "+"-".repeat(vida1));
-                System.out.println(jugador2+":"+vida2+" "+"-".repeat(vida2));
 
-                if (vida1 <= 0) break;
+                if (vida1 <= 0){
+                break;}
 
                 // Turno del jugador 1
+                System.out.println(jugador1 + ":" + vida1 + " " + "-".repeat(vida1));
+                System.out.println(jugador2 + ":" + vida2 + " " + "-".repeat(vida2));
 
-                System.out.println(jugador1+":"+vida1+" "+"-".repeat(vida1));
-                System.out.println(jugador2+":"+vida2+" "+"-".repeat(vida2));
-
-                d1 = (ataque1 -defensa2+ random.nextInt(10));
+                d1 = Math.max(5, ataque1 - defensa2 / 2 + random.nextInt(30));
                 vida2 -= d1;
                 System.out.println(jugador1 + " ataca a " + jugador2 + " causando " + d1 + " de daño. Vida restante de " + jugador2 + ": " + Math.max(0, vida2));
 
-                System.out.println(jugador1+":"+vida1+" "+"-".repeat(vida1));
-                System.out.println(jugador2+":"+vida2+" "+"-".repeat(vida2));
 
             }
         }
-        if (vida1>0){
-            System.out.println("El ganador es:"+jugador1);
-        }else{
-            System.out.println("El ganador es:"+jugador2);
+            if (vida1 == 0) {
+                System.out.println("El ganador es:" + jugador1);
+            } else {
+                System.out.println("El ganador es:" + jugador2);
+
         }
-    }
-    }
     }
 }
 
