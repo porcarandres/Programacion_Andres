988d3c6368439e90d0751bed48607dc6fdafdabd
porcarandres <porcarandres07@gmail.com>
2025-11-12 12:51:38 +0100

    inicial 488898

---
+++ src/Tema3_ProgramacionModular/Tema3Ejercicio7.java
@@ -0,0 +1,39 @@
+package Tema3_ProgramacionModular;
+
+import java.util.Scanner;
+
+public class Tema3Ejercicio7 {
+    public static void main(String[] args){
+        System.out.println("Pon numeros positivos y si quieres acabar pon 0: ");
+        Scanner sc = new Scanner(System.in);
+        int numero;
+
+        do {
+            numero = sc.nextInt();
+
+            if (numero > 0) {
+                if (esPrimo(numero)) {
+                    System.out.println(numero + " es primo.");
+                } else {
+                    System.out.println(numero + " no es primo.");
+                }
+            } else if (numero < 0) {
+                System.out.println("Por favor, introduce solo números positivos.");
+            }
+
+        } while (numero != 0);
+
+        System.out.println("Programa finalizado.");
+    }
+    public static boolean esPrimo(int n) {
+        if (n <= 1) return false;
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
