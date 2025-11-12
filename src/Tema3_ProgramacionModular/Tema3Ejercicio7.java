dde75fd767412dacdc36e2c5e29865e529f808f6
porcarandres <porcarandres07@gmail.com>
2025-11-12 12:53:58 +0100

    inicial 488898

--- src/Tema3_ProgramacionModular/Tema3Ejercicio7.java
+++ src/Tema3_ProgramacionModular/Tema3Ejercicio7.java
@@ -26,9 +26,11 @@ public class Tema3Ejercicio7 {
         System.out.println("Programa finalizado.");
     }
     public static boolean esPrimo(int n) {
-        if (n <= 1) return false;
+        if (n <= 1) {
+            return false;
+        }
 
-        for (int i = 2; i <= Math.sqrt(n); i++) {
+        for (int i = 2; i <= n / 2; i++) {
             if (n % i == 0) {
                 return false;
             }
