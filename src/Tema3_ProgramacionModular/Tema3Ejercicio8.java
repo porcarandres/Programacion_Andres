3c18465355ea325520b01066f2e074ec659edc84
porcarandres <porcarandres07@gmail.com>
2025-11-12 13:42:59 +0100

    inicial 488898

--- src/Tema3_ProgramacionModular/Tema3Ejercicio8.java
+++ src/Tema3_ProgramacionModular/Tema3Ejercicio8.java
@@ -3,50 +3,54 @@ package Tema3_ProgramacionModular;
 import java.util.Scanner;
 
 public class Tema3Ejercicio8 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Introduce el día (1-31): ");
-        int dia = sc.nextInt();
-
-        System.out.print("Introduce el mes (1-12): ");
-        int mes = sc.nextInt();
-
-        System.out.print("Introduce el año: ");
-        int anio = sc.nextInt();
-
-        if (fechaValida(dia, mes, anio)) {
-            System.out.println("La fecha es correcta.");
-        } else {
-            System.out.println("La fecha NO es correcta.");
-        }
-
+    public static void main(String[]args){
+        ComprFecha();
     }
-    public static boolean fechaValida(int dia, int mes, int anio) {
-        if (mes < 1 || mes > 12) {
-            return false;
-        }
+    public static int DiasMes(int mes, int anio){
 
-        int diasMes;
-
-        switch (mes) {
+        switch (mes){
             case 1: case 3: case 5: case 7: case 8: case 10: case 12:
-                diasMes = 31;
-                break;
+                return 31;
             case 4: case 6: case 9: case 11:
-                diasMes = 30;
-                break;
+                return 30;
             case 2:
-                diasMes = esBisiesto(anio) ? 29 : 28;
-                break;
+                if (EsBisiesto(anio)) {
+                    return 29;
+                } else {
+                    return 28;
+                }
             default:
-                return false;
+                return 0;
         }
-
-        return dia >= 1 && dia <= diasMes;
     }
 
-    public static boolean esBisiesto(int anio) {
+    public static boolean esFechaValida(int dia, int mes, int anio){
+        if (mes < 1 || mes > 12) return false;
+        int DiasMax = DiasMes(mes,anio);
+        return dia >= 1 && dia <= DiasMax;
+    }
+
+    public static void ComprFecha(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Dime un dia:");
+        int dia = scanner.nextInt();
+
+        System.out.println("Dime un mes:");
+        int mes = scanner.nextInt();
+
+        System.out.println("Dime un año");
+        int anio = scanner.nextInt();
+
+        boolean valida = esFechaValida(dia,mes,anio);
+
+        if (valida){
+            System.out.println("La fecha es valida.");
+        }else{
+            System.out.println("La fecha no es valida");
+        }
+    }
+    public static boolean EsBisiesto(int anio) {
         return (anio % 4 == 0 && anio % 100 != 0) || (anio % 400 == 0);
     }
 }
