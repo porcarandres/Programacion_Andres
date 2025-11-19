ac9149de182deac3d7c373e157634ded5c8048f9
porcarandres <porcarandres07@gmail.com>
2025-11-19 13:09:15 +0100

    inicial 488898

---
+++ src/Tema3_ProgramacionModular/MyMath.java
@@ -0,0 +1,23 @@
+package Tema3_ProgramacionModular;
+public class MyMath {
+    public static double squarePerimeter(double lado) {
+        return 4 * lado;
+    }
+    public static double squareArea(double lado) {
+        return lado * lado;
+    }
+    public static double rectanglePerimeter(double base, double altura) {
+        return 2 * (base + altura);
+    }
+    public static double rectangleArea(double base, double altura) {
+        return base * altura;
+    }
+    public static double circlePerimeter(double radio) {
+        return 2 * Math.PI * radio;
+    }
+    public static double circleArea(double radio) {
+        return Math.PI * radio * radio;
+    }
+
+}
+
