a0563e23c5b5eeffd8a48ef09cd4eb857ee5e0a4
porcarandres <porcarandres07@gmail.com>
2025-11-27 10:53:43 +0100

    inicial 488898

--- src/Tema3_ProgramacionModular/MyArray.java
+++ src/Tema3_ProgramacionModular/MyArray.java
@@ -1,4 +1,17 @@
 package Tema3_ProgramacionModular;
 
 public class MyArray {
+
+    public static void mostrarArray(int[] gerom){
+        for (int i = 0; i < gerom.length; i++) {
+            System.out.println(gerom[i]);
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args){
+        int[] gerom = new int[]{0,1,2,3,4};
+        mostrarArray(gerom);
+
+    }
 }
