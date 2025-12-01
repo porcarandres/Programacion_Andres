b6eb3a5ced777d81e56f32fccfb7285f0991b7c4
porcarandres <porcarandres07@gmail.com>
2025-12-01 09:16:30 +0100

    inicial 488898

--- src/Tema3_ProgramacionModular/MyArray.java
+++ src/Tema3_ProgramacionModular/MyArray.java
@@ -4,29 +4,36 @@ import java.util.Scanner;
 
 public class MyArray {
 
-    public static void mostrarArray(int[] gerom){
-        for (int i = 0; i < gerom.length; i++) {
-            System.out.println(gerom[i]);
+    public static void mostrarArray(int[] a){
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
         }
         System.out.println();
     }
 
-    public static int maxArray(int[] gerom){
-        int max = gerom[0];
-        for (int i = 0; i < gerom.length; i++) {
-            if(gerom[i] > max){
-                max =gerom[i];
+    public static int maxArray(int[] a){
+        int max = a[0];
+        for (int i = 0; i < a.length; i++) {
+            if(a[i] > max){
+                max = a[i];
             }
         }return max;
     }
 
-    public static int minArray(int[] gerom){
-        int min = gerom[0];
-        for (int i = 0; i < gerom.length; i++) {
-            if(gerom[i] < min){
-                min =gerom[i];
+    public static int minArray(int[] a){
+        int min = a[0];
+        for (int i = 0; i < a.length; i++) {
+            if(a[i] < min){
+                min = a[i];
             }
         }return min;
+    }
+
+    public static double mediaArray(int[] a) {
+        int suma = 0;
+        for (int i = 0; i < a.length; i++) {
+            suma += a[i];
+        } return (double) suma / a.length;
     }
 
     public static void main(String[] args){
@@ -35,16 +42,19 @@ public class MyArray {
         int opcion = sc.nextInt();
 
         switch (opcion){
-            case 1: int[] gerom = new int[]{3, 7, 2, 9, 5};
-                mostrarArray(gerom);
+            case 1: int[] n1 = new int[]{3, 7, 2, 9, 5};
+                mostrarArray(n1);
                 break;
-            case 2: int[] numero = new int[]{3, 7, 2, 9, 5};
-                System.out.println("Máximo array {3, 7, 2, 9, 5}: " + maxArray(numero));
+            case 2: int[] n2 = new int[]{3, 7, 2, 9, 5};
+                System.out.println("Máximo array {3, 7, 2, 9, 5}: " + maxArray(n2));
                 break;
-            case 3: int[] n = new int[]{3,7,2,9,5};
-                System.out.println("Minimo array {3, 7, 2, 9, 5}: "+ minArray(n));
+            case 3: int[] n3 = new int[]{3,7,2,9,5};
+                System.out.println("Minimo array {3, 7, 2, 9, 5}: "+ minArray(n3));
                 break;
-
+            case 4: int[] n4 = new int[]{3,7,2,9,5};
+                System.out.println("Media array {3, 7, 2, 9, 5}: " + mediaArray(n4));
+                break;
+            case 5:
         }
 
     }
@@ -53,6 +63,7 @@ public class MyArray {
         System.out.println("1 - Mostrar Array");
         System.out.println("2 - Max Array");
         System.out.println("3 - Min Array");
+        System.out.println("4 - Media Array");
         System.out.println("Elige una opcion: ");
     }
 }
