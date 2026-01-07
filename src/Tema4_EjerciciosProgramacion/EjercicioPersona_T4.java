6dd27bbf0298ecd27dd93f66b58825afa73d5e9f
porcarandres <porcarandres07@gmail.com>
2026-01-07 13:00:28 +0100

    inicial 488898

---
+++ src/Tema4_EjerciciosProgramacion/EjercicioPersona_T4.java
@@ -0,0 +1,57 @@
+package Tema4_EjerciciosProgramacion;
+
+import java.util.Scanner;
+
+public class EjercicioPersona_T4 {
+
+    Scanner sc = new Scanner(System.in);
+
+    public class Persona {
+        public static final int Default_Age= 0;
+        String nombre;
+        int edad;
+
+        public void saludar() {
+            System.out.println("¡Hola!");
+        }
+
+        public void presentarme() {
+            System.out.println("Soy " + nombre + " y tengo " + edad + " años.");
+        }
+
+        public void saludarA(String nombre) {
+            System.out.println("¡Hola " + nombre + "!");
+        }
+
+    public Persona(String nombre){
+        this.nombre="";
+        this.edad= Default_Age;
+
+    }
+    public Persona(String nombre, int edad){
+        this.nombre="";
+        this.edad= 0;
+    }
+    public int getEdad (){
+            return edad;
+    }
+    public String getNombre (){
+            return nombre;
+    }
+    public void setEdad(int edad){
+        if(edad>0) this.edad=edad;
+    }
+    public void setNombre(String nombre){
+        this.nombre=nombre;
+    }
+    public void showInfo(){
+        System.out.println("Edad: "+edad);
+        System.out.println("Nombre: "+nombre);
+    }
+    }
+    public static void main(String[] args){
+        System.out.println();
+    }
+}
+
+
