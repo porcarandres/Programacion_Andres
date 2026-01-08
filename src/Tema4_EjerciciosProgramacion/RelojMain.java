275d44dee8be79e7006a0fb1e9ccca47f93c28ed
porcarandres <porcarandres07@gmail.com>
2026-01-08 12:17:54 +0100

    inicial 488898

---
+++ src/Tema4_EjerciciosProgramacion/RelojMain.java
@@ -0,0 +1,15 @@
+package Tema4_EjerciciosProgramacion;
+
+import java.util.Scanner;
+
+public class RelojMain {
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+
+    int h;
+    int m;
+    int s;
+
+
+    }
+}
