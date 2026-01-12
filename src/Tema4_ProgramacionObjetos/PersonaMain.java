ba77221b9dd9851bc975b75c5e85c2dad40a85f1
andporgui <porcarandres07@gmail.com>
2026-01-12 10:52:34 +0100

    1 comit

--- src/Tema4_ProgramacionObjetos/PersonaMain.java
+++ src/Tema4_ProgramacionObjetos/PersonaMain.java
@@ -1,4 +1,17 @@
 package Tema4_ProgramacionObjetos;
 
 public class PersonaMain {
+    public static void main(String[] args) {
+        Persona p1 = new Persona();
+        p1.setDNI();
+        p1.setNombre();
+        p1.setApellido1();
+        p1.setApellido2();
+        p1.setEdad();
+
+        Persona p2 = new Persona();
+
+
+
+    }
 }
