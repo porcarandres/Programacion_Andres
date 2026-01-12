900f61a3467f62bae7ff081330d53c13e8ca2e03
andporgui <porcarandres07@gmail.com>
2026-01-12 09:05:48 +0100

    1 comit

--- src/Tema4_ProgramacionObjetos/Reloj.java
+++ src/Tema4_ProgramacionObjetos/Reloj.java
@@ -2,7 +2,7 @@ package Tema4_ProgramacionObjetos;
 
 public class Reloj {
 
-    int hora, minuto, segundo;
+    int hora, minuto, segundo, hora12;
     boolean formato24h;
 
     public static final int DefaultSegundo = 0;
@@ -18,17 +18,21 @@ public class Reloj {
         return hora;
     }
     public void setHora(int hora) {
-        if (hora >= 0 && hora <= 23) {
+        if (hora >= 0 && hora <= 24) {
             this.hora = hora;
         } else {
-            System.out.println("Hora invalida");
+            if(hora >=0 && hora <= 12){
+            this.hora = hora;
+            } else if(hora>= 12 && hora<=24){
+                this.hora12 = hora -12;
+            }
         }
     }
     public int getMinuto() {
         return minuto;
     }
     public void setMinuto(int minuto) {
-        if (minuto >= 0 && minuto <= 59) {
+        if (minuto >= 0 && minuto <= 60) {
             this.minuto = minuto;
         } else {
             System.out.println("Minuto invalido");
@@ -38,7 +42,7 @@ public class Reloj {
         return segundo;
     }
     public void setSegundo(int segundo) {
-        if (segundo >= 0 && segundo <= 59) {
+        if (segundo >= 0 && segundo <= 60) {
             this.segundo = segundo;
         } else {
             System.out.println("Segundo invalido");
@@ -47,14 +51,15 @@ public class Reloj {
     public boolean isFormato24h() {
         return formato24h;
     }
-    public void setFormato24h(boolean formato24h) {
-        this.formato24h = formato24h;
-    }
     public void mostrarHora() {
-        if (formato24h) {
-            System.out.println("Hora formato 24h"+hora+":"+minuto+":"+segundo);
-        } else {
-
+        if (formato24h == true) {
+            System.out.println("Hora formato 24h: "+hora+":"+minuto+":"+segundo);
+        } else if(formato24h != true) {
+            if (hora <= 12){
+                System.out.println("Hora :"+hora+":"+minuto+":"+segundo+"AM");
+            }else{
+                System.out.println("Hora :"+hora12+":"+minuto+":"+segundo+"PM   ");
+            }
         }
     }
 }
---
+++ src/Tema4_ProgramacionObjetos/RelojMain.java
@@ -0,0 +1,4 @@
+package Tema4_ProgramacionObjetos;
+
+public class RelojMain {
+}
