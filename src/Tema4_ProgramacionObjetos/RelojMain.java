da85135569c1c3228d9421c85903de08abf40f01
andporgui <porcarandres07@gmail.com>
2026-01-12 09:31:49 +0100

    1 comit

--- src/Tema4_ProgramacionObjetos/RelojMain.java
+++ src/Tema4_ProgramacionObjetos/RelojMain.java
@@ -3,10 +3,10 @@ package Tema4_ProgramacionObjetos;
 public class RelojMain {
     public static void main(String[] args){
         Reloj reloj = new Reloj();
-        reloj.setHora(0);
-        reloj.setMinuto(2);
-        reloj.setSegundo(2);
-        reloj.setFormato24h(true);
+        reloj.setHora(17);
+        reloj.setMinuto(50);
+        reloj.setSegundo(40);
+        reloj.setFormato24h(false);
         reloj.mostrarHora();
     }
 }
