f62fe02f475cdc77e9457d47eaba3673b52563f7
porcarandres07 <porcarandres07@gmail.com>
2026-01-22 12:36:41 +0100

    inicial 4

---
+++ src/Tema4_ProgramacionObjetos/Hero.java
@@ -0,0 +1,23 @@
+package Tema4_ProgramacionObjetos;
+
+public class Hero {
+    private String nombre;
+    private int level;
+    private int health;
+    private int maxHealth;
+    private int experience;
+    private int attack;
+    private int defense;
+
+    public Hero(String nombre, int level, int health, int maxHealth, int experience, int attack, int defense){
+        this.nombre=nombre;
+        this.level=level;
+        this.health=health;
+        this.maxHealth=maxHealth;
+        this.experience=experience;
+        this.attack=attack;
+        this.defense=defense;
+    }
+
+
+}
