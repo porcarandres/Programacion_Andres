9946107c985ba1c59344cfc728bbd42cac05ba77
porcarandres07 <porcarandres07@gmail.com>
2026-02-02 09:50:17 +0100

    inicial 4

--- src/Tema4_ProgramacionObjetos/PersonaCuenta.java
+++ src/Tema4_ProgramacionObjetos/PersonaCuenta.java
@@ -11,9 +11,33 @@ public class PersonaCuenta {
             this.cuentas = new Cuenta[3];
             this.contadorCuentas = 0;
         }
+        public PersonaCuenta(){
+            this.dni= "1111111";
+            this.cuentas= new Cuenta[3];
+            this.contadorCuentas= 0;
+        }
 
         public String getDni() {
             return dni;
         }
 
+        public boolean añadirCuenta(Cuenta nuevaCuenta){
+            if(contadorCuentas< cuentas.length){
+                cuentas[contadorCuentas] = nuevaCuenta;
+                contadorCuentas ++;
+                return true;
+            } else {
+                System.out.println("Error "+dni+" ya tiene 3 cuentas.");
+                return false;
+            }
+        }
+        public boolean esMoroso(){
+            for (int i = 0; i < contadorCuentas; i++) {
+                if (cuentas[i].setSaldoDispo() < 0){
+                    return true;
+                }
+
+            }
+            return false;
+        }
 }
