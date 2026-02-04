5a44c2c62bcf30c5fc6050ea00de5416d7a57258
porcarandres07 <porcarandres07@gmail.com>
2026-02-04 12:56:30 +0100

    inicial 4

--- src/Tema5_ProgramacionColecciones/GestionGym.java
+++ src/Tema5_ProgramacionColecciones/GestionGym.java
@@ -2,25 +2,52 @@ package Tema5_ProgramacionColecciones;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class GestionGym {
-    Map<String, Integer> Gym = new HashMap<>();
+    public static void main(String[] args) {
+        GestionGym gestion = new GestionGym();
+        gestion.darAlta();
+    }
 
+    Map<String, UsuarioGym> Gym = new HashMap<>();
+    Scanner sc = new Scanner(System.in);
 
+    public void darAlta(){
+        System.out.println("DNI del nuevo usuario:");
+        String dni = sc.nextLine();
 
+        System.out.println("Edad:");
+        int edad = sc.nextInt();
+        sc.nextLine();
+        Gym.put(dni, new UsuarioGym(dni, edad));
+        System.out.println("Dado de alta");
 
-    public class UsuariosGym {
+    }
+    public void darBaja(){
+        System.out.println("--- Baja de Usuario ---");
+        System.out.println("Introduce el DNI del usuario a borrar:");
+        String dni = sc.nextLine();
+
+        if (Gym.containsKey(dni)) {
+            Gym.remove(dni);
+            System.out.println("Usuario con DNI " + dni + " eliminado.");
+        } else {
+            System.out.println("No se encontró ningún usuario con ese DNI.");
+        }
+    }
+    public class UsuarioGym {
 
         private String dni;
         private int edad;
 
-        public UsuariosGym(String dni, int edad) {
+        public UsuarioGym(String dni, int edad) {
             this.dni = dni;
             this.edad = edad;
 
         }
 
-        public UsuariosGym() {
+        public UsuarioGym() {
             this.dni = "11111111A";
             this.edad = 18;
         }
@@ -42,3 +69,7 @@ public class GestionGym {
         }
     }
 }
+
+
+
+
