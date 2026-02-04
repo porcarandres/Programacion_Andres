57c98c3efae8f8a96b67d6580ea7c97dc2f64b29
porcarandres07 <porcarandres07@gmail.com>
2026-02-04 13:23:28 +0100

    inicial 4

--- src/Tema5_ProgramacionColecciones/GestionGym.java
+++ src/Tema5_ProgramacionColecciones/GestionGym.java
@@ -6,9 +6,40 @@ import java.util.Scanner;
 
 public class GestionGym {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         GestionGym gestion = new GestionGym();
-        gestion.darAlta();
-        gestion.darBaja();
+
+        System.out.println("1-Dar Alta");
+        System.out.println("1-Dar Baja");
+        System.out.println("3-Mostrar Datos");
+        System.out.println("4-Modificar Usuarios");
+        System.out.println("5-Salir");
+
+        int opcion;
+
+        do {
+            System.out.println("Inserta opcion:");
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    gestion.darAlta();
+                    break;
+                case 2:
+                    gestion.darBaja();
+                    break;
+                case 3:
+                    gestion.mostrarDatos();
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    System.out.println("salir");
+                    break;
+                default:
+                    System.out.println("Opcion no valida");
+            }
+        } while (opcion != 5);
     }
 
     Map<String, UsuarioGym> Gym = new HashMap<>();
@@ -36,6 +67,29 @@ public class GestionGym {
             System.out.println("No hay ningun usuario con ese DNI.");
         }
     }
+
+    public void mostrarDatos(){
+        System.out.println("Datos Usuarios:");
+        for (Map.Entry<String, UsuarioGym> entry : Gym.entrySet()) {
+            String dni = entry.getKey();
+            UsuarioGym usuario = entry.getValue();
+            System.out.println("DNI: " + dni + " | Edad: " + usuario.getEdad());
+        }
+    }
+
+     public void modificarUsuarios(){
+         System.out.println("DNI del usuario a modificar:");
+         String dni = sc.nextLine();
+
+         if (Gym.containsKey(dni)){
+             int edadnueva = sc.nextInt();
+             sc.nextLine();
+
+             Gym.replace(dni,new UsuarioGym(dni,edadnueva));
+         }else {
+             System.out.println("El DNI no existe.");
+         }
+     }
 
 
     public class UsuarioGym {
