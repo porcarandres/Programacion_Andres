bb21c1fb553b4c4523899e4232c8a8ee2d828cf3
porcarandres07 <porcarandres07@gmail.com>
2026-02-04 12:30:59 +0100

    inicial 4

---
+++ src/Tema5_ProgramacionColecciones/GestionGym.java
@@ -0,0 +1,44 @@
+package Tema5_ProgramacionColecciones;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GestionGym {
+    Map<String, Integer> Gym = new HashMap<>();
+
+
+
+
+    public class UsuariosGym {
+
+        private String dni;
+        private int edad;
+
+        public UsuariosGym(String dni, int edad) {
+            this.dni = dni;
+            this.edad = edad;
+
+        }
+
+        public UsuariosGym() {
+            this.dni = "11111111A";
+            this.edad = 18;
+        }
+
+        public int getEdad() {
+            return edad;
+        }
+
+        public String getDni() {
+            return dni;
+        }
+
+        public void setDni(String dni) {
+            this.dni = dni;
+        }
+
+        public void setEdad(int edad) {
+            this.edad = edad;
+        }
+    }
+}
