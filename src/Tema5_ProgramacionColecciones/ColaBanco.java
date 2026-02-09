27e664c974c1d1694b63817ae99556f4b27fc528
porcarandres07 <porcarandres07@gmail.com>
2026-02-09 08:47:53 +0100

    inicial 4

--- src/Tema5_ProgramacionColecciones/ColaBanco.java
+++ src/Tema5_ProgramacionColecciones/ColaBanco.java
@@ -6,8 +6,39 @@ import java.util.Scanner;
 
 public class ColaBanco {
     public static void main(String[] args){
+        ColaBanco ColaBanco = new ColaBanco();
         Scanner sc= new Scanner(System.in);
+        int opcion = 0;
 
+        // Necesitamos un bucle para que el menú se repita
+        while (opcion != 4) {
+            System.out.println("1. Añadir usuario");
+            System.out.println("2. Atender Primero");
+            System.out.println("3. Eliminar Usuario");
+            System.out.println("4. Salir");
+            System.out.println("   Opcion:");
+
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    ColaBanco.añadirUsuario();
+                    break;
+                case 2:
+                    ColaBanco.atenderPrimero();
+                    break;
+                case 3:
+                    System.out.println("Introduce la posicion a eliminar:");
+                    int pos = sc.nextInt();
+                    ColaBanco.quitarDeCualquierPosicion(pos);
+                    break;
+                case 4:
+                    System.out.println("Salir");
+                    break;
+                default:
+                    System.out.println("Opcion no valida");
+            }
+        }
     }
     List<UsuarioBanco> cola = new ArrayList<UsuarioBanco>();
     Scanner sc= new Scanner(System.in);
@@ -22,6 +53,23 @@ public class ColaBanco {
 
         cola.add(new UsuarioBanco(nombre,dni,edad));
         System.out.println("Datos añadidos:"+nombre+" Dni: "+dni+" Edad: "+edad);
+    }
+
+    public void atenderPrimero() {
+        if (!cola.isEmpty()) {
+            UsuarioBanco atendido = cola.remove(0);
+            System.out.println("Atendiendo a: " + atendido.getNombre());
+        } else {
+            System.out.println("La cola está vacía.");
+        }
+    }
+
+    public void quitarDeCualquierPosicion(int posicion){
+        if (posicion >= 0 && posicion < cola.size()){
+            cola.remove(posicion);
+        }else {
+            System.out.println("Esa posicion no esta en la cola.");
+        }
     }
 
     public class UsuarioBanco{
@@ -66,3 +114,4 @@ public class ColaBanco {
         }
     }
 }
+
