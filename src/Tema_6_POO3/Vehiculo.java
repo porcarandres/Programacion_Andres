6fa3dbb61c3e04b5f458acb65613ebb8942d5511
porcarandres07 <porcarandres07@gmail.com>
2026-02-19 10:57:14 +0100

    inicial 4

--- src/Tema_6_POO3/Vehiculo.java
+++ src/Tema_6_POO3/Vehiculo.java
@@ -1,5 +1,16 @@
 package Tema_6_POO3;
 
 public class Vehiculo {
+    private String matricula;
+    private String marca;
+    private String modelo;
+    private int anioMatriculacion;
+
+    public Vehiculo(String matricula, String marca, String modelo, int anioMatriculacion) {
+        this.matricula = matricula;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.anioMatriculacion = anioMatriculacion;
+    }
 
 }
