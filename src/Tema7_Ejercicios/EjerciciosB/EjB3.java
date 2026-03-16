4a391852fdf69bf61e2d6b736ba7c83d69793b3b
porcarandres07 <porcarandres07@gmail.com>
2026-03-16 10:45:33 +0100

    inicial 4

---
+++ src/Tema7_Ejercicios/EjerciciosB/EjB3.java
@@ -0,0 +1,12 @@
+package Tema7_Ejercicios.EjerciciosB;
+
+import java.io.File;
+
+public class EjB3 {
+    String rutaA = "";
+    String rutaB = "";
+
+    File archivoA= new File(rutaA);
+    File archivoB= new File(rutaB);
+
+}
